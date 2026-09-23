136fa83969c7be1b14f5d6199f38aab5ddf76f41
Usuario Generico <usuario@git.local>
2026-09-22 21:28:56 -0500

    julian

---
+++ 1.py
@@ -0,0 +1,8 @@
+lenguajes = ("r", "java","pytho","pseint","julia")
+print(len(lenguajes))
+print(lenguajes[0])
+print(lenguajes[-1])
+    
+    
+
+
---
+++ 2.py
@@ -0,0 +1,8 @@
+edades = (12,20,18,80,1,17,22,8,30,11)
+mayor = 18
+for edad in edades:
+    mayor=mayor+1
+print("promedio de edades",edades)
+
+
+
